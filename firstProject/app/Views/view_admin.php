706f513ef8ae0ece7ca22f70086f0084a1fc8ace
VinySilvia <vinysilvia1771@gmail.com>
2021-10-21 15:57:36 -1200

    sixth commit

--- firstProject/app/Views/view_admin.php
+++ firstProject/app/Views/view_admin.php
@@ -27,7 +27,7 @@
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
       <img src="/assets/adminlte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">Blog App</span>
     </a>
 
     <!-- Sidebar -->
@@ -38,7 +38,7 @@
           <img src="/assets/adminlte/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">VinyS</a>
         </div>
       </div>
 
